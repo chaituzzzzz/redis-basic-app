7890a555be0bc48ba841197c461ffceb6c93c6b1
chaituzzzzz <kchaitanya819@gmail.com>
2020-06-25 02:13:41 +0530

    redis

--- index.js
+++ index.js
@@ -9,20 +9,19 @@ const client = redis.createClient(6379);
 
 //fetches the repos by the sorting order of full name
 app.get('/repos', async (req,res)=>{
-    const repodata = 'githubrepodata'
+    const repodata = 'githubpublicrepodata'
     client.get(repodata,async (err, data) => {
         if (err) throw err;   
         if (data !== null) {
           res.send(JSON.parse(data));
-        } else {
-            let response = await fetch('https://api.github.com/search/repositories?q=javascript');
-            const data = await response.json();
-            var responseData = data.items.sort((a, b) => (a.full_name > b.full_name) ? 1 : -1)
-            responseData = responseData.map(x=> _.pick(x,'id','node_id','name','full_name','private'))
-            client.setex(repodata,3600,JSON.stringify(responseData))
-            res.send(responseData);
         }
-      });    
+      });
+      let response = await fetch('https://api.github.com/search/repositories?q=javascript');
+      const data = await response.json();
+      var responseData = data.items.sort((a, b) => (a.full_name > b.full_name) ? 1 : -1)
+      responseData = responseData.map(x=> _.pick(x,'id','node_id','name','full_name','private'))
+      client.setex(repodata,3600,JSON.stringify(responseData))
+      res.send(responseData);          
 })
 
 app.listen(PORT, () =>{
